41211fe5068f3215a3d74bbe2412ce758e1b1fa9
Josh-Ay <ayo.oloyede16@gmail.com>
2022-05-25 23:09:10 +0100

    Remove valid-url library

--- src/validators/validator.js
+++ src/validators/validator.js
@@ -1,42 +1,40 @@
 import { toast } from 'react-toastify';
-import validUrl from 'valid-url';
 
 const checkEmptyString = (str) => {    // check that a label was entered
     if (str === "") {
         return toast.error("Please enter a label", {position: "top-center", hideProgressBar: true, pauseOnHover:false, autoClose:4000});
-    } else {
-        return false;
     }
+
+    return false;
 }
 
 const checkUrlisValid = (str) => { // check validity of the url entered
-    if (validUrl.isUri(str)){
-        const image_url = new Image();  // creating a new image object to verify that the url is an actual image
-        image_url.src = str;
-        if (image_url.width > 0){
-            return true;
-        }else{
-            return false;
-        };
-    }else{
+    
+    try {
+        
+        let newURL = new URL(str);
+        return true;
+
+    } catch (error) {
         return false;
     }
+
 }
 
 const checkPassword = (str) => {   // check password match
     if (str === "password123") {
         return true;
-    } else {
-        return false;
     }
+
+    return false;
 }
 
 const emptyImages = (listToCheck) => { // function to check if there are currently no images
     if (listToCheck.length < 1){
       return true;
-    }else{
-      return false;
     }
+    
+    return false;
 };
 
 export {checkEmptyString, checkUrlisValid, checkPassword, emptyImages};
